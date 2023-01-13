98ae1bde52280fbfbb1e20f6d6b0fdea4cda2461
Gerardo Castillo <gerrcass@gmail.com>
2023-01-13 13:43:08 -0400

    🚀Initializing project

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+    return (
+        <footer class="attribution">
+            Challenge by
+            <a
+                href="https://gerardocastillo.me/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Frontend Mentor
+            </a>
+            . Coded by
+            <a
+                href="https://gerardocastillo.me/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                @gerrcass
+            </a>
+            .
+        </footer>
+    )
+}
+
+export { Footer }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Frontend Mentor Challenge" />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="./images/favicon-32x32.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
